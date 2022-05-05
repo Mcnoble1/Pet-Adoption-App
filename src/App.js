df796bda2924c132096e41d149962a5691a69bc6
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-05-05 02:02:12 +0100

    setup js tools:octocat:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from "react";
+import { render } from "react-dom";
+
 const Pet = (props) => {
-    return (
-        React.createElement("div", {}, [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h2", {}, props.breed),
-        ])
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
 
-
 const App = () => {
-    return (
-        React.createElement('div', null, [
-            React.createElement('h1', null, 'Adopt Me'),
-            React.createElement(Pet, {name: 'Camila', animal: 'Dog', breed: 'Husky'}),
-            React.createElement(Pet, {name: 'Rescue', animal: 'Cat', breed: 'Persian'}),
-            React.createElement(Pet, {name: 'Bingo', animal: 'Bird', breed: 'Labrador'}),
-        ])
-    )
-}
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+  return React.createElement("div", null, [
+    React.createElement("h1", null, "Adopt Me"),
+    React.createElement(Pet, { name: "Camila", animal: "Dog", breed: "Husky" }),
+    React.createElement(Pet, {
+      name: "Rescue",
+      animal: "Cat",
+      breed: "Persian",
+    }),
+    React.createElement(Pet, {
+      name: "Bingo",
+      animal: "Bird",
+      breed: "Labrador",
+    }),
+  ]);
+};
+render(React.createElement(App), document.getElementById("root"));
